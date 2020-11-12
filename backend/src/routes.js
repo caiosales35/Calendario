cbc9f4204a32577e17866061848b1eb1b5c52983
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-11-12 17:46:31 -0300

    Rotas de criacao e listagem de usuarios

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
 const routes = express.Router();
 
-routes.get('/', (request, response) => {
-    return response.send('Iniciando API');
-});
+const UserController = require('./controllers/UserController');
+
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.create);
 
 module.exports = routes;
