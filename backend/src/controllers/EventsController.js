9f17a63a16afb25602f4d98533e677a7d25ca9aa
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-11-13 18:48:56 -0300

    Rotas de remocao e listagem de eventos

--- backend/src/controllers/EventsController.js
+++ backend/src/controllers/EventsController.js
@@ -3,7 +3,8 @@ const { index } = require('./UserController');
 
 module.exports = {
     async index(request, response) {
-        const events = await connection('events').select('*');
+        const user_id = request.headers.authorization;
+        const events = await connection('events').where('user_id', user_id).select('*');
         return response.json(events);
     },
 
@@ -28,6 +29,18 @@ module.exports = {
             return response.json({id});    
         }
         return response.status(400).json({error: 'Já existe evento no horário selecionado!'});
-        
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const user_id = request.headers.authorization;
+
+        const event = await connection('events').where('id', id).select('user_id').first();
+        if (event.user_id != user_id) {
+            return response.status(401).json({error: 'Operação não permitida!'});
+        }
+        await connection('events').where('id', id).delete();
+        return response.status(204).send();
     }
+
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,10 +4,13 @@ const routes = express.Router();
 const UserController = require('./controllers/UserController');
 const EventsController = require('./controllers/EventsController');
 
+
+
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.create);
 
 routes.get('/events', EventsController.index);
 routes.post('/events', EventsController.create);
+routes.delete('/events/:id', EventsController.delete);
 
 module.exports = routes;
