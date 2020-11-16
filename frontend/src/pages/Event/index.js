85531bd7cc9e5dd48be60abda21f4114ceb0bb74
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-11-16 19:56:42 -0300

    Integrando interface de cadastro de eventos ao backend

--- frontend/src/pages/Event/index.js
+++ frontend/src/pages/Event/index.js
@@ -1,13 +1,47 @@
+import { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker'
 /* Docs https://github.com/wojtekmaj/react-datetime-picker */
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import './styles.css';
 
 import logoImg from '../../assets/logo.png';
 
 export default function Event() {
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [start, setStart] = useState(new Date());
+    const [end, setEnd] = useState(new Date());
+
+    const userId = localStorage.getItem("userId");
+    const history = useHistory();
+
+    async function handleNewEvent(e) {
+        e.preventDefault();
+        const data = {
+            title,
+            description,
+            start,
+            end,
+            userId
+        }
+
+        try {
+            await api.post("events", data, { 
+                headers: {
+                    Authorization: userId
+                }
+             });
+             alert("Evento cadastrado!");
+             history.push("/profile");
+        } catch (err) {
+            alert("ERRO! Verifique se já não existe evento no mesmo horário");
+        }
+    }
+
     return (
         <div className="event-container">
             <div className="content">
@@ -19,30 +53,40 @@ export default function Event() {
                         Voltar
                     </ Link>
                 </section>
-                <form>
-                    <input placeholder="Titulo do evento" />
-                    <textarea placeholder="Descrição" />
-                    <DateTimePicker 
-                        className="datetimepicker" 
-                        clearIcon="" 
-                        format="y-MM-dd HH:mm"
-                        minDate={new Date()}
-                        value={new Date()}
-                        calendarIcon="Início"
+                <form onSubmit={handleNewEvent}>
+                    <input 
+                        placeholder="Titulo do evento" 
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
+                    />
+                    <textarea 
+                        placeholder="Descrição" 
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
                     />
                     <DateTimePicker 
                         className="datetimepicker" 
                         clearIcon="" 
                         format="y-MM-dd HH:mm"
                         minDate={new Date()}
-                        value={new Date()}
+                        value={start}
+                        calendarIcon="Início"
+                        onChange={e => setStart(e)}
+                    />
+                    <DateTimePicker 
+                        className="datetimepicker" 
+                        clearIcon="" 
+                        format="y-MM-dd HH:mm"
+                        minDate={new Date()}
+                        value={end}
                         calendarIcon="Fim"
+                        onChange={e => setEnd(e)}
                     />
                     { /* 
                     <input type="text" placeholder="Inicio (AAAA-MM-DD HH:MM 24h)" />
                     <input type="text" placeholder="Fim (AAAA-MM-DD HH:MM 24h" />
-                    <button type="submit" className="button" >Cadastrar evento</button>
                     */ }
+                    <button type="submit" className="button" >Cadastrar evento</button>
                 </form>
             </div>
         </div>
