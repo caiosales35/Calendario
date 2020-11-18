b55ff018336906f038a333f4db5f21e5d1ab9e32
Caio Cesar de Morais Sales <caiosales35@gmail.com>
2020-11-18 14:07:22 -0300

    Bloqueando rotas no frontend - caso nao esteja logado

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,9 +1,19 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import { isAuthenticated } from './services/auth';
 
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Event from './pages/Event';
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => (
+        isAuthenticated() ? 
+            <Component {...props} /> 
+            : 
+            <Redirect to={{pathname: "/", state: { from: props.location } }} />
+    )} />
+);
 
 export default function Routes() {
     return (
@@ -11,9 +21,9 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
-                <Route path="/profile" component={Profile} />
-                <Route path="/event/new" component={Event} />
-                <Route path="/event/edit/:id" component={Event} />
+                <PrivateRoute path="/profile" component={Profile} />
+                <PrivateRoute path="/event/new" component={Event} />
+                <PrivateRoute path="/event/edit/:id" component={Event} />
             </Switch>
         </BrowserRouter>
     )
